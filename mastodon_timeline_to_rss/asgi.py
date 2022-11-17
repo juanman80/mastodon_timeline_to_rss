859ea1ed9cebbf9997fd3d7134b4c2c07a9b7c49
Juanma <juanman80@gmail.com>
2022-11-17 17:21:26 +0000

    initial django project

---
+++ mastodon_timeline_to_rss/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for mastodon_timeline_to_rss project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mastodon_timeline_to_rss.settings')
+
+application = get_asgi_application()
